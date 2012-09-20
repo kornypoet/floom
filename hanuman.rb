7a7bdb75dcdcee4393b52fca135364c1f3a2aae0
Travis Dempsey <dempsey.travis@gmail.com>
2012-09-20 18:12:19 -0500

    cheap way to move files around

---
+++ hanuman.rb
@@ -0,0 +1,13 @@
+
+require 'gorillib/string/inflections'
+
+require 'hanuman/interpreter'
+require 'hanuman/stage'              ## base object for building blocks
+
+
+# require 'hanuman/slot'               ## coordinate connections
+# require 'hanuman/slottable'          ## coordinate connections
+
+# require 'hanuman/action'             ## represents a transformation of products
+# require 'hanuman/product'            ## represents a product
+# require 'hanuman/graph'              ## contains stages
